a3b35a494e21f48a29e2cc40b523c97d911eca12
Brian Oh <ohbrian@Brians-MacBook-Pro.local>
2019-06-26 15:50:39 -0400

    added index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,14 +5,67 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      pins: [
+        { pinNumber: 1, round: true, target: false },
+        { pinNumber: 2, round: true, target: false },
+        { pinNumber: 3, round: true, target: false },
+        { pinNumber: 4, round: true, target: false },
+        { pinNumber: 5, round: true, target: false },
+        { pinNumber: 6, round: true, target: false },
+        { pinNumber: 7, round: true, target: false },
+        { pinNumber: 8, round: true, target: false },
+        { pinNumber: 9, round: true, target: false },
+        { pinNumber: 10, round: true, target: false }
+      ],
+      round: "start",
+      subRound: null
     };
+    this.onClickStart = this.onClickStart.bind(this);
+  }
 
+  componentDidMount() {
+    // this.randomPinSelector();
+  }
+
+  randomPinSelector() {
+    let randomPinIndex = Math.floor(Math.random() * this.state.pins.length);
+    let newState = Object.assign({}, this.state);
+    newState.pins[randomPinIndex].target = true;
+
+    this.setState({ newState });
+  }
+
+  onClickStart() {
+    if (this.state.round === "start") {
+      this.setState({
+        round: 1,
+        subRound: "first"
+      })
+    } else if (this.state.round === 10 && this.state.subRound === "second") {
+      this.setState({
+        round: "start",
+        subRound: null
+      })
+    } else if (this.state.subRound === "first") {
+      this.setState({
+        subRound: "second"
+      })
+    } else {
+      this.setState({
+        round: this.state.round += 1,
+        subRound: "first"
+      })
+    } 
   }
 
   render() {
     return (
       <div className="bowling">
-        hello
+        <div>{this.state.pins.map((pin, i) => {})}</div>
+        <div>
+          <button onClick={this.onClickStart}>{this.state.round}</button>
+          <div>you are on : {this.state.subRound}</div>
+        </div>
       </div>
     );
   }
